161a4debb0c0c650435704a429f736d661c79df8
nafila <nafilatk7@gamil.com>
2025-11-09 22:32:49 +0530

    repush

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -12,7 +12,7 @@ export default async function ProductsPage() {
 
   return (
     <main className="min-h-screen bg-gray-100 py-10 px-8">
-      <h1 className="text-4xl font-bold text-center mb-10">All Products 🛍️</h1>
+      <h1 className="text-4xl font-bold text-center mb-10">All Products</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {products.map((product: any) => (
